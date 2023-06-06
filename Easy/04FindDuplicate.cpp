a94eeeb1b72a0403e6d54a06642f1c49e13f9707
Chandan Kushwaha <chandan7979ck@gmail.com>
2023-06-06 11:12:34 +0530

    easy

---
+++ Easy/04FindDuplicate.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int findDuplicate(vector<int> &arr, int n)
+{
+	sort(arr.begin(),arr.end());
+
+        for (int i = 0; i < n - 1; i++) {
+          if (arr[i] == arr[i + 1])
+            return arr[i];
+        }
+}
+
+
+int main()
+{
+    vector<int> arr = {1,3,4,2,4,5,6};
+    int n = arr.size();
+
+    cout<<findDuplicate(arr, n)<<endl;
+
+    return 0;
+}
